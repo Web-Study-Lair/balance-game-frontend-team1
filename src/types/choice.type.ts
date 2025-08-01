b85a34df3e2ff75a0e42c100a4065634a1f1e35f
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-08-01 14:44:48 +0900

    type들 class로 바꾸고 생성자 추가 추후 타입 처리를 원활히 하기 위함

--- src/types/choice.type.ts
+++ src/types/choice.type.ts
@@ -1,5 +1,12 @@
-export interface Choice {
-  id: number;
-  imageUrl: string;
-  description: string;
+export class Choice {
+  id!: number;
+  imageUrl!: string;
+  description!: string;
+
+  constructor(params: Choice) {
+    const { id, imageUrl, description } = params;
+    this.id = id;
+    this.imageUrl = imageUrl;
+    this.description = description;
+  }
 }
