14c94235d3f9d3140bf799de7376d1343c130ba7
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-08-01 15:48:59 +0900

    게임 페이지 UI 요소를 util과 연결 구현

--- src/game-play/game-play.component.tsx
+++ src/game-play/game-play.component.tsx
@@ -16,7 +16,7 @@ export function GamePlay(props: Game) {
   const { title, choices } = props;
   return (
     <div className="BalanceGamePlay">
-      <h1 className="BalanceGameTitle">{title}</h1>
+      <h2 className="BalanceGameTitle">{title}</h2>
       <div>
         {choices.map((choice) => (
           <GameChoiceBox key={choice.id} {...choice} />
