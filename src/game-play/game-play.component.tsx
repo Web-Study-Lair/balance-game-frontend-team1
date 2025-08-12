bcd8f6035c51a7aa6d52bd59e86860665e1d897e
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-08-12 18:00:59 +0900

    dimmed layer 구현

--- src/game-play/game-play.component.tsx
+++ src/game-play/game-play.component.tsx
@@ -1,29 +1,77 @@
 import { Game } from "../types/game.type";
 import { Choice } from "../types/choice.type";
 import { chooseChoice } from "./game-play.util";
+import { useState } from "react";
 
-export function GameChoiceBox(props: Choice & { gameId: number }) {
-  const { gameId, id, imageUrl, description } = props;
+export function GameChoiceBox(
+  props: Choice & {
+    gameId: number;
+    isGameFinished: boolean;
+    setIsGameFinished: React.Dispatch<React.SetStateAction<boolean>>;
+  }
+) {
+  const {
+    gameId,
+    id,
+    imageUrl,
+    description,
+    isGameFinished,
+    setIsGameFinished,
+  } = props;
+
+  const dimmedLayer = (
+    <div
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        background: "rgba(0, 0, 0, 0.5)",
+        zIndex: 10,
+      }}
+    />
+  );
+
   return (
-    // TODO: onClick 이벤트 이후 결과 출력 UI 구현
     <div
       className="BalanceGameChoiceBox"
-      onClick={() => chooseChoice(gameId, id)}
+      style={{
+        position: "relative",
+        cursor: isGameFinished ? "default" : "pointer",
+      }}
     >
-      <img className="BalanceGameChoiceBoxImage" src={imageUrl} />
-      <p className="BalanceGameChoiceBoxDescription">{description}</p>
+      <div
+        onClick={() => {
+          if (!isGameFinished) {
+            chooseChoice(gameId, id);
+            setIsGameFinished(true);
+          }
+        }}
+      >
+        <img className="BalanceGameChoiceBoxImage" src={imageUrl} />
+        <p className="BalanceGameChoiceBoxDescription">{description}</p>
+        {isGameFinished ? dimmedLayer : null}
+      </div>
     </div>
   );
 }
 
 export function GamePlay(props: Game) {
   const { id, title, choices } = props;
+  const [isGameFinished, setIsGameFinished] = useState(false);
   return (
     <div className="BalanceGamePlay">
       <h1 className="BalanceGameTitle">{title}</h1>
       <div className="BalanceGameChoiceBoxes">
         {choices.map((choice) => (
-          <GameChoiceBox key={choice.id} gameId={id} {...choice} />
+          <GameChoiceBox
+            key={choice.id}
+            gameId={id}
+            isGameFinished={isGameFinished}
+            setIsGameFinished={setIsGameFinished}
+            {...choice}
+          />
         ))}
       </div>
     </div>
