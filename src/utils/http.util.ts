75282b10ce890345275de8820e0d0f6f88aecea2
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-09-05 19:32:03 +0900

    http 레이어 구현방식 수정

--- src/utils/http.util.ts
+++ src/utils/http.util.ts
@@ -2,12 +2,21 @@ import axios from "axios";
 
 export class HttpUtil {
   async get(url: string) {
-    const result = await axios.get(url);
+    const result = await axios.request({
+      method: "get",
+      url,
+      headers: {},
+    });
     return result;
   }
 
   async post(url: string, data: object) {
-    const result = await axios.post(url, data);
+    const result = await axios.request({
+      method: "post",
+      url,
+      data,
+      headers: {},
+    });
     return result;
   }
 }
