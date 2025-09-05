c10d64974ec6fbf68235c0111e6ea053f8fa9d10
ushape08 <85924465+ushape08@users.noreply.github.com>
2025-09-05 19:17:59 +0900

    localhost에서 127.0.0.1로 수정

--- src/utils/config.util.ts
+++ src/utils/config.util.ts
@@ -1,4 +1,4 @@
 export class ConfigUtil {
   SERVER_URL: string =
-    import.meta.env.REACT_APP_BACKEND_SERVER_URL || "localhost:3000";
+    import.meta.env.REACT_APP_BACKEND_SERVER_URL || "127.0.0.1:3000";
 }
